448ea40a1315bb21683dd858ff03f7561ea973bc
aumcintyre <aumcintyre@gmail.com>
2022-11-16 22:15:10 -0600

    connection issues

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Student-Grades';
 
-connect(connectionString, {
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
